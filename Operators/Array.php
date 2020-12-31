2a869c52c1f88b59b15b0d4c3912c88250316541
sasithorn <mint251710@gmail.com>
2020-12-31 15:53:34 +0700

    PHP

---
+++ Operators/Array.php
@@ -0,0 +1,41 @@
+<?php
+$x = array("a" => "red", "b" => "green");  
+$y = array("c" => "blue", "d" => "yellow");  
+
+print_r($x + $y); // union of $x and $y
+?>
+<br>
+<?php
+$x = array("a" => "red", "b" => "green");  
+$y = array("c" => "blue", "d" => "yellow");  
+
+var_dump($x == $y);
+?> 
+<br>
+<?php
+$x = array("a" => "red", "b" => "green");  
+$y = array("c" => "blue", "d" => "yellow");  
+
+var_dump($x === $y);
+?>  
+<br>
+<?php
+$x = array("a" => "red", "b" => "green");  
+$y = array("c" => "blue", "d" => "yellow");  
+
+var_dump($x != $y);
+?>
+<br>
+<?php
+$x = array("a" => "red", "b" => "green");  
+$y = array("c" => "blue", "d" => "yellow");  
+
+var_dump($x <> $y);
+?> 
+<br>
+<?php
+$x = array("a" => "red", "b" => "green");  
+$y = array("c" => "blue", "d" => "yellow");  
+
+var_dump($x !== $y);
+?> 
---
+++ Operators/String.php
@@ -0,0 +1,14 @@
+<?php
+$txt1 = "Hello";
+$txt2 = " world!";
+echo $txt1 . $txt2;
+?> 
+
+<br>
+
+<?php
+$txt1 = "Hello";
+$txt2 = " world!";
+$txt1 .= $txt2;
+echo $txt1;
+?> 
